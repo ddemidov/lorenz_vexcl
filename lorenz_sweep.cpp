f3f1a6e4c736ebde6b7af02da5adfdba2cf39557
Denis Demidov <dennis.demidov@gmail.com>
2014-05-23 22:55:20 +0400

    Record lengths of kneading sequences

--- lorenz_sweep.cpp
+++ lorenz_sweep.cpp
@@ -16,7 +16,7 @@ namespace odeint = boost::numeric::odeint;
 //---------------------------------------------------------------------------
 // Generate a monolythic kernel that does a single Runge-Kutta step.
 //---------------------------------------------------------------------------
-vex::generator::Kernel<10> make_kernel(const vex::Context &ctx) {
+vex::generator::Kernel<11> make_kernel(const vex::Context &ctx) {
     typedef vex::symbolic<double>       sym_vector;
     typedef boost::array<sym_vector, 3> sym_state;
 
@@ -45,6 +45,7 @@ vex::generator::Kernel<10> make_kernel(const vex::Context &ctx) {
 
     sym_vector k(sym_vector::VectorParameter);
     sym_vector q(sym_vector::VectorParameter);
+    vex::symbolic<int> m(vex::symbolic<int>::VectorParameter);
 
     // Stepper type
     odeint::runge_kutta_dopri5<
@@ -69,9 +70,11 @@ vex::generator::Kernel<10> make_kernel(const vex::Context &ctx) {
         "  if ((" << dx_new[1] << " < 0) && (" << x_new[0] << " > 0)) {\n"
         "    " << k << " += " << q << ";\n"
         "    " << q << " *= " << config::q << ";\n"
+        "    " << m << " += 1;\n"
         "  } else\n"
         "  if ((" << dx_new[1] << " > 0) && (" << x_new[0] << " < 0)) {\n"
         "    " << q << " *= " << config::q << ";\n"
+        "    " << m << " += 1;\n"
         "  }\n"
         "}\n";
 
@@ -86,66 +89,81 @@ vex::generator::Kernel<10> make_kernel(const vex::Context &ctx) {
 
     // Generate the kernel from the recorded sequence
     return vex::generator::build_kernel(ctx, "lorenz_sweep", body.str(),
-            x[0], x[1], x[2], dx[0], dx[1], dx[2], k, q, alpha, lambda);
+            x[0], x[1], x[2], dx[0], dx[1], dx[2], k, q, m, alpha, lambda);
 }
 
 //---------------------------------------------------------------------------
 int main(int argc, char *argv[]) {
-    config::read(argc, argv);
-    const int n = config::alpha_steps * config::lambda_steps;
+    try {
+        config::read(argc, argv);
+        const int n = config::alpha_steps * config::lambda_steps;
 
-    // Initialize VexCL context
-    vex::Context ctx( vex::Filter::Env && vex::Filter::DoublePrecision );
-    std::cout << ctx << std::endl;
+        // Initialize VexCL context
+        vex::Context ctx( vex::Filter::Env && vex::Filter::DoublePrecision );
+        std::cout << ctx << std::endl;
 
-    // Fill parameters.
-    double ha = (config::alpha_max  - config::alpha_min ) / (config::alpha_steps  - 1);
-    double hl = (config::lambda_max - config::lambda_min) / (config::lambda_steps - 1);
+        // Fill parameters.
+        double ha = (config::alpha_max  - config::alpha_min ) / (config::alpha_steps  - 1);
+        double hl = (config::lambda_max - config::lambda_min) / (config::lambda_steps - 1);
 
-    vex::vector<double> alpha (ctx, n);
-    vex::vector<double> lambda(ctx, n);
+        vex::vector<double> alpha (ctx, n);
+        vex::vector<double> lambda(ctx, n);
 
-    alpha  = config::alpha_min  + ha * (vex::element_index() % config::alpha_steps);
-    lambda = config::lambda_min + hl * (vex::element_index() / config::alpha_steps);
+        alpha  = config::alpha_min  + ha * (vex::element_index() % config::alpha_steps);
+        lambda = config::lambda_min + hl * (vex::element_index() / config::alpha_steps);
 
-    // Set initial position.
-    vex::vector<double> x(ctx, n);
-    vex::vector<double> y(ctx, n);
-    vex::vector<double> z(ctx, n);
+        // Set initial position.
+        vex::vector<double> x(ctx, n);
+        vex::vector<double> y(ctx, n);
+        vex::vector<double> z(ctx, n);
 
-    vex::vector<double> dx(ctx, n);
-    vex::vector<double> dy(ctx, n);
-    vex::vector<double> dz(ctx, n);
+        vex::vector<double> dx(ctx, n);
+        vex::vector<double> dy(ctx, n);
+        vex::vector<double> dz(ctx, n);
 
-    x = config::x0;
-    y = config::y0;
-    z = config::z0;
+        x = config::x0;
+        y = config::y0;
+        z = config::z0;
 
-    dx = 0;
-    dy = 0;
-    dz = 0;
+        dx = 0;
+        dy = 0;
+        dz = 0;
 
-    // Kneading
-    vex::vector<double> k(ctx, n);
-    vex::vector<double> q(ctx, n);
+        // Kneading
+        vex::vector<double> k(ctx, n);
+        vex::vector<double> q(ctx, n);
+        vex::vector<int>    m(ctx, n);
 
-    k = -1;
-    q = 1;
+        k = -1;
+        q =  1;
+        m =  0;
 
-    // Create kernel.
-    auto lorenz = make_kernel(ctx);
+        // Create kernel.
+        auto lorenz = make_kernel(ctx);
 
-    // Integrate over time.
-    for(double time = 0; time < config::tmax; time += config::dt) {
-        lorenz(x, y, z, dx, dy, dz, k, q, alpha, lambda);
-    }
+        // Integrate over time.
+        for(double time = 0; time < config::tmax; time += config::dt) {
+            lorenz(x, y, z, dx, dy, dz, k, q, m, alpha, lambda);
+        }
 
-    std::vector<double> kneading(n);
-    vex::copy(k, kneading);
-    std::ofstream f("kneading.dat");
-    for(int j = 0, idx = 0; j < config::lambda_steps; ++j) {
-        for(int i = 0; i < config::alpha_steps; ++i, ++idx)
-            f << kneading[idx] << " ";
-        f << "\n";
+        vex::Reductor<int, vex::MIN> min(ctx);
+        vex::Reductor<int, vex::MAX> max(ctx);
+
+        std::cout << "Number of kneading points: " << min(m) << " - " << max(m) << std::endl;
+
+        std::vector<double> kneading(n);
+        vex::copy(k, kneading);
+        std::ofstream f("kneading.dat");
+        for(int j = 0, idx = 0; j < config::lambda_steps; ++j) {
+            for(int i = 0; i < config::alpha_steps; ++i, ++idx)
+                f << kneading[idx] << " ";
+            f << "\n";
+        }
+    } catch (const vex::backend::error &e) {
+        std::cerr << "VexCL error: " << e << std::endl;
+        return 1;
+    } catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
     }
 }
